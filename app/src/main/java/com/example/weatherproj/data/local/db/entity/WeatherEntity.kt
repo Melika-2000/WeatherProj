be54bd435fa0db004971d76a9aab59daab1dfae9
admin <melika.7990@gmail.com>
2022-08-31 11:44:06 +0430

    Weather Entity initialized

--- app/src/main/java/com/example/weatherproj/data/local/db/entity/WeatherEntity.kt
+++ app/src/main/java/com/example/weatherproj/data/local/db/entity/WeatherEntity.kt
@@ -1,8 +1,19 @@
 package com.example.weatherproj.data.local.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
 
-@Entity
+@Entity(tableName = "weather")
 data class WeatherEntity(
-
+    @ColumnInfo(name = "city_id") @PrimaryKey val cityId: Int,
+    @ColumnInfo(name = "main") val main: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "temperature") val temperature: Float? = null,
+    @ColumnInfo(name = "humidity_percent") val humidityPercent: Int? = null,
+    @ColumnInfo(name = "win_speed") val windSpeed: Int? = null,
+    @ColumnInfo(name = "cloudiness_percent") val cloudinessPercent: Int? = null,
+  //  @ColumnInfo(name = "time") val time: Calendar
 )
+
