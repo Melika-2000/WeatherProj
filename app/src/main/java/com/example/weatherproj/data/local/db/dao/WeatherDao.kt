14b9a6fdea22e27ef746e065a336c80cd3d481a7
admin <melika.7990@gmail.com>
2022-08-31 13:52:28 +0430

    Weather Dao initialized

--- app/src/main/java/com/example/weatherproj/data/local/db/dao/WeatherDao.kt
+++ app/src/main/java/com/example/weatherproj/data/local/db/dao/WeatherDao.kt
@@ -1,7 +1,24 @@
 package com.example.weatherproj.data.local.db.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import com.example.weatherproj.data.local.db.entity.CityAndWeather
+import com.example.weatherproj.data.local.db.entity.WeatherEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherDao {
+
+    @Query("SELECT * FROM weather WHERE weather_id=:weatherId")
+    fun getWeather(weatherId: Int): Flow<WeatherEntity>
+
+    @Transaction
+    @Query("SELECT * FROM city WHERE city_id IN (SELECT DISTINCT(weather_id) FROM weather)")
+    fun getWeatherOfCities(): Flow<List<CityAndWeather>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeather(weather: WeatherEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeather(weathers: List<WeatherEntity>)
+
 }
